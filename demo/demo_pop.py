3f266d3ae5641261545580e3200767cd9986c0e4
Adamdad <787707188@qq.com>
2020-01-30 21:45:50 -0800

    add

---
+++ demo/demo_pop.py
@@ -0,0 +1,206 @@
+import sys
+sys.path.append('../')
+
+from scipy import io
+from PIL import Image
+import numpy as np
+import os
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
+# this makes our figures bigger
+pylab.rcParams['figure.figsize'] = 20, 12
+
+from maskrcnn_benchmark.config import cfg
+from maskrcnn_benchmark.structures.bounding_box import BoxList
+from demo.predictor import COCODemo
+
+
+def load(path):
+    """
+    Given an url of an image, downloads the image and
+    returns a PIL image
+    """
+    pil_image = Image.open(path).convert("RGB")
+    # convert to BGR format
+    image = np.array(pil_image)[:, :, [2, 1, 0]]
+    return image
+
+def imshow(img):
+    plt.figure()
+    plt.imshow(img[:, :, [2, 1, 0]])
+    plt.axis("off")
+    plt.show()
+
+
+def boxlist_nms(boxlist, nms_thresh=0.5, max_proposals=-1, score_field="scores"):
+    """
+    Performs non-maximum suppression on a boxlist, with scores specified
+    in a boxlist field via score_field.
+
+    Arguments:
+        boxlist(BoxList)
+        nms_thresh (float)
+        max_proposals (int): if > 0, then only the top max_proposals are kept
+            after non-maximum suppression
+        score_field (str)
+    """
+    if nms_thresh <= 0:
+        return boxlist
+    mode = boxlist.mode
+    boxlist = boxlist.convert("xyxy")
+    boxes = boxlist.bbox
+    score = boxlist.get_field(score_field)
+    keep, picked_score = nms(boxes, score, nms_thresh)
+    if max_proposals > 0:
+        keep = keep[: max_proposals]
+    boxlist = boxlist[keep]
+    return boxlist.convert(mode)
+
+
+"""
+    Non-max Suppression Algorithm
+    @param list  Object candidate bounding boxes
+    @param list  Confidence score of bounding boxes
+    @param float IoU threshold
+    @return Rest boxes after nms operation
+"""
+
+
+def nms_candidate(bounding_boxes, confidence_score, threshold):
+    # If no bounding boxes, return empty list
+    if len(bounding_boxes) == 0:
+        return []
+
+    # Bounding boxes
+    boxes = np.array(bounding_boxes)
+
+    # coordinates of bounding boxes
+    start_x = boxes[:, 0]
+    start_y = boxes[:, 1]
+    end_x = boxes[:, 2]
+    end_y = boxes[:, 3]
+
+    # Confidence scores of bounding boxes
+    score = np.array(confidence_score)
+
+    # Compute areas of bounding boxes
+    areas = (end_x - start_x + 1) * (end_y - start_y + 1)
+
+    # Sort by confidence score of bounding boxes
+    order = np.argsort(score)
+    candidates = []
+    # Iterate bounding boxes
+    while order.size > 0:
+        # The index of largest confidence score
+        index = order[-1]
+
+        # Compute ordinates of intersection-over-union(IOU)
+        x1 = np.maximum(start_x[index], start_x[order[:-1]])
+        x2 = np.minimum(end_x[index], end_x[order[:-1]])
+        y1 = np.maximum(start_y[index], start_y[order[:-1]])
+        y2 = np.minimum(end_y[index], end_y[order[:-1]])
+
+        # Compute areas of intersection-over-union
+        w = np.maximum(0.0, x2 - x1 + 1)
+        h = np.maximum(0.0, y2 - y1 + 1)
+        intersection = w * h
+
+        # Compute the ratio between intersection and union
+        ratio = intersection / (areas[index] + areas[order[:-1]] - intersection)
+
+        nonleft = np.where(ratio >= threshold)
+
+        candidate = np.append(order[nonleft], index)
+
+        candidates.append(candidate)
+
+        left = np.where(ratio < threshold)
+        order = order[left]
+
+    return candidates
+
+
+"""
+    Non-max Suppression Algorithm
+    @param list  Object candidate bounding boxes
+    @param list  Confidence score of bounding boxes
+    @param float IoU threshold
+    @return Rest boxes after nms operation
+"""
+
+
+def nms(bounding_boxes, confidence_score, threshold):
+    # If no bounding boxes, return empty list
+    if len(bounding_boxes) == 0:
+        return [], []
+
+    # Bounding boxes
+    boxes = np.array(bounding_boxes)
+
+    # coordinates of bounding boxes
+    start_x = boxes[:, 0]
+    start_y = boxes[:, 1]
+    end_x = boxes[:, 2]
+    end_y = boxes[:, 3]
+
+    # Confidence scores of bounding boxes
+    score = np.array(confidence_score)
+
+    # Picked bounding boxes
+    picked_boxes = []
+    picked_score = []
+
+    # Compute areas of bounding boxes
+    areas = (end_x - start_x + 1) * (end_y - start_y + 1)
+
+    # Sort by confidence score of bounding boxes
+    order = np.argsort(score)
+
+    # Iterate bounding boxes
+    while order.size > 0:
+        # The index of largest confidence score
+        index = order[-1]
+
+        # Pick the bounding box with largest confidence score
+        picked_boxes.append(index)
+        picked_score.append(confidence_score[index])
+
+        # Compute ordinates of intersection-over-union(IOU)
+        x1 = np.maximum(start_x[index], start_x[order[:-1]])
+        x2 = np.minimum(end_x[index], end_x[order[:-1]])
+        y1 = np.maximum(start_y[index], start_y[order[:-1]])
+        y2 = np.minimum(end_y[index], end_y[order[:-1]])
+
+        # Compute areas of intersection-over-union
+        w = np.maximum(0.0, x2 - x1 + 1)
+        h = np.maximum(0.0, y2 - y1 + 1)
+        intersection = w * h
+
+        # Compute the ratio between intersection and union
+        ratio = intersection / (areas[index] + areas[order[:-1]] - intersection)
+
+        left = np.where(ratio < threshold)
+        order = order[left]
+
+    return picked_boxes, picked_score
+
+config_file = "../configs/caffe2/e2e_mask_rcnn_R_50_FPN_1x_caffe2.yaml"
+
+# update the config options with the config file
+cfg.merge_from_file(config_file)
+# manual override some options
+cfg.merge_from_list(["MODEL.DEVICE", "cuda"])
+coco_demo = COCODemo(
+    cfg,
+    min_image_size=800,
+    confidence_threshold=0,
+)
+file_dir = 'E:\Matlab2019b\POP\popValImages\popValImages'
+
+for fn in os.listdir(file_dir)[:10]:
+    img_file = os.path.join(file_dir,fn)
+    image = load(img_file)
+
+    predictions = coco_demo.run_on_opencv_image(image)
+    imshow(predictions)
